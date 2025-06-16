b563e98f959a5c4446ed338f01a6d99aa26ac098
Nitjsefn <>
2025-06-16 15:28:34 +0200

    added basic features to site

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,25 +2,26 @@ import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Login from './pages/Login'
 import { useState } from 'react'
-import Context from './support/Context';
+//import Context from './support/Context';
 import Register from './pages/Register';
 import Home from './pages/Home';
+import Overview from './pages/Overview';
 
 function App() {
-    const ctx = useState(new Context)[0];
+    //const ctx = useState(new Context)[0];
+    const [token, setToken] = useState("");
 
     return (
         <div className="min-h-screen bg-gray-100">
-            <Header />
+            <Header setToken={setToken}/>
             <div className="p-6">
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/login" element={<Login ctx={ctx}/>} />
-                    <Route path="/register" element={<Register ctx={ctx}/>} />
-                    <Route path="/overview" element={null} />
+                    <Route path="/login" element={<Login setToken={setToken}/>} />
+                    <Route path="/register" element={<Register setToken={setToken}/>} />
+                    <Route path="/overview" element={<Overview token={token}/>} />
                 </Routes>
             </div>
-            {ctx.tokenExists().toString()}
         </div>
     )
 }
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -9,12 +9,13 @@ import { Link } from 'react-router-dom'
   //</header>
 //)
 
-const Header = () => (
+const Header = (props: {setToken: (t: string)=>void}) => (
   <header className="bg-blue-700 text-white px-6 py-4 flex gap-6 text-lg font-medium">
     <Link to="/" className="hover:underline">🏠 Home</Link>
     <Link to="/Overview" className="hover:underline">📄 Overview</Link>
     <Link to="/login" className="hover:underline">➕ Login</Link>
     <Link to="/register" className="hover:underline">➕ Register</Link>
+    <span onClick={() => props.setToken("")}>Log Out</span>
   </header>
 )
 
